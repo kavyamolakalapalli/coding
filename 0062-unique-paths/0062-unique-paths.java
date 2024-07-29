2936b38fec84ee7de7530604095c36f38b5605b2
kavyamolakalapalli <115872200+kavyamolakalapalli@users.noreply.github.com>
2024-07-29 20:08:08 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (38.87%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,7 +1,7 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int N = m+n-2;
-        int R = m-1;
+        int R = n-1;
         double sum = 1;
         for(int i = 1;i<=R;i++){
             sum = sum*(N-R+i)/i;
