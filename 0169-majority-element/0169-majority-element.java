a749716c34c34bc53f8ee12daecf47740f196af4
kavyamolakalapalli <115872200+kavyamolakalapalli@users.noreply.github.com>
2024-09-22 20:19:35 +0530

    Time: 2 ms (76.55%), Space: 53.2 MB (21.75%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -15,17 +15,7 @@ class Solution {
                 count ++;
             }
         }
-        int count1 = 0;
-        for(int i = 0;i < n;i++){
-            if(nums[i] == ele){
-                count1++;
-            }
-        }
         
-        if(count1 > n/2){
-            return ele;
-        }
-        
-        return -1;
+        return ele;
     }
 }
