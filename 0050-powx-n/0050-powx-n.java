af6e9332eba3faf625c17a7ddcba50391d09c85c
kavyamolakalapalli <115872200+kavyamolakalapalli@users.noreply.github.com>
2024-10-01 09:58:53 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (24.83%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,18 @@
 class Solution {
+    public static double pow_util(double x,long n){
+        if(n == 0) return 1.0;
+        if(n == 1) return x;
+        if(n < 0) return pow_util(1/x,-n);
+        double result = pow_util(x*x,n/2);
+        if(n%2 != 0){
+            result *= x;
+        }
+        
+        return result;
+    }
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        // return Math.pow(x,n);
+        return (pow_util(x,n));
+        
     }
 }
