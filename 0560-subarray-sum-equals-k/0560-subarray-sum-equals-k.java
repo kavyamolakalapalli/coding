47cbd6abc218748e8291a521e310f99fde5b493f
kavyamolakalapalli <115872200+kavyamolakalapalli@users.noreply.github.com>
2024-10-05 18:50:24 +0530

    Time: 24 ms (75.26%), Space: 46.2 MB (64.93%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,22 +1,21 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
-        int n = nums.length; // size of the given array
+        int n = nums.length; 
         Map<Integer, Integer> mpp = new HashMap<>();
         int preSum = 0, cnt = 0;
 
-        mpp.put(0, 1); // Setting 0 in the map.
+        mpp.put(0, 1); 
         for (int i = 0; i < n; i++) {
-            // Add current element to prefix sum:
+            
             preSum += nums[i];
 
-            // Calculate x - k:
+           
             int remove = preSum - k;
 
-            // Add the number of subarrays to be removed:
-            cnt += mpp.getOrDefault(remove, 0); // This will now work as expected.
+            
+            cnt += mpp.getOrDefault(remove, 0); 
 
-            // Update the count of prefix sum in the map.
             mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
         }
         return cnt;
